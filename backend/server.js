270aea173bfb47f2340551c155ba05654ead6ae8
jhamilton14 <jshamilton1414@gmail.com>
2021-07-09 21:16:05 -0400

    docker final

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const port = 5000; // desired port
 
-mongoose.connect(process.env.DATABASE_URL_DOCKER, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('connected to db'))
 	.catch(error => console.error(error));
 
